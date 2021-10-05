017bcb7ef3b4c5d2cef6aa740b793886aa495e43
Valentin Varbanov <valentin.varbanov@gmail.com>
2021-10-05 19:09:56 +0300

    Second class views and models

---
+++ wombat/crypto/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField() # price in USD
+    code = models.CharField(max_length=3) # e.g. BTC ETH
--- wombat/wombat/settings.py
+++ wombat/wombat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crypto.apps.CryptoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
