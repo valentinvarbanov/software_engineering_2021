5ca5962f87e3c0ecc146b4513a9806ad24c6b7c4
Valentin Varbanov <valentin.varbanov@gmail.com>
2021-10-12 19:57:28 +0300

    Add model changed needed for homework

--- wombat/crypto/models.py
+++ wombat/crypto/models.py
@@ -6,6 +6,7 @@ class Currency(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField() # price in USD
     code = models.CharField(max_length=3) # e.g. BTC ETH
+    supply = models.PositiveIntegerField(default=10000000) # 10 000 000
 
     def __str__(self):
         return (self.code + " " + str(self.price))
