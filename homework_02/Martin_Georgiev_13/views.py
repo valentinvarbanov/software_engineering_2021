67e5278d96a306c146b9f735e7f1e2ddd3ef56ae
marto55 <martin_mu@abv.bg>
2021-10-24 20:56:28 +0300

    I <3 trains

---
+++ homework_02/Martin_Georgiev_13/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import requests
+import pytz
+import datetime
+
+def trains(request):
+
+    cur_time = datetime.datetime.now(pytz.timezone('America/New_York'))
+    response = requests.get("https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=-departure_time&filter%5Bstop%5D=place-north").json()
+    statuses = []
+    for status in response['data']:
+        statuses.append(status['attributes']['status'])
+
+    context = {
+        "cur_year" : cur_time.year,
+        "cur_date" : cur_time.day,
+        "cur_month" : cur_time.month,
+        "cur_time" : cur_time.strftime("%H:%M:%S"),
+        "statuses" : statuses,
+        'data': response,
+    }
+    return render(request, "trains.html", context)
+
