39764fa141027b41a73ae90d873b89bd2f7694a7
Stelian <steliantodorichkov03@gmail.com>
2021-10-31 00:02:34 +0300

    Add tracks and optimize for loop

--- homework_02/stelian_todorichkov_18/views.py
+++ homework_02/stelian_todorichkov_18/views.py
@@ -9,7 +9,7 @@ def board(request):
     params = {
         'page[offset]': '0',
         'page[limit]': '10',
-        'include': 'trip,schedule',
+        'include': 'trip,schedule,stop',
         'filter[stop]': 'place-north',
         'filter[route_type]': '2', # rail
         'filter[direction_id]': '0', # only departure
@@ -19,27 +19,36 @@ def board(request):
 
     list = []
     
-    # get statuses
+    # get statuses, trip and stop ids
     for data in response['data']:
         dict = {}
         dict['status'] = data['attributes']['status']
-        dict['id'] = data['relationships']['trip']['data']['id']
+        dict['trip_id'] = data['relationships']['trip']['data']['id']
+        dict['stop_id'] = data['relationships']['stop']['data']['id']
         list.append(dict)
     
     for data in response['included']:
         for el in list:
-            if data['type'] == 'trip' and el['id'] == data['id']:
+            if data['type'] == 'trip' and el['trip_id'] == data['id']:
                 # get train ids
                 el['train'] = data['attributes']['name']
                 #get destinations
                 el['destination'] = data['attributes']['headsign']
-            if data['type'] == 'schedule' and el['id'] in data['id']:
+                break
+            if data['type'] == 'schedule' and el['trip_id'] in data['id']:
                 #get departure times
                 el['time'] = data['attributes']['departure_time']
+                break
+            if data['type'] == 'stop' and el['stop_id'] == data['id']:
+                if data['attributes']['platform_code'] is None:
+                   el['track'] = 'TBD'
+                else: 
+                    el['track'] = data['attributes']['platform_code']
+
 
     # sort list by time
     list = sorted(list, key = lambda i: i['time'])
-    
+
     # format time 
     for el in list:
         el['time'] = datetime.fromisoformat(el['time']).strftime('%I:%M %p')
