7b24fda2b2c6518a4547775c7a7cf94432a4c034
Stelian <steliantodorichkov03@gmail.com>
2021-10-30 20:43:54 +0300

    Little update Homework 2 Stelian Todorichkov 18

--- homework_02/stelian_todorichkov_18/views.py
+++ homework_02/stelian_todorichkov_18/views.py
@@ -12,7 +12,7 @@ def board(request):
         'include': 'trip,schedule',
         'filter[stop]': 'place-north',
         'filter[route_type]': '2', # rail
-        'filter[direction_id]': 0, # only departure
+        'filter[direction_id]': '0', # only departure
     }
 
     response = requests.get('https://api-v3.mbta.com/predictions', params=params).json()
@@ -23,21 +23,19 @@ def board(request):
     for data in response['data']:
         dict = {}
         dict['status'] = data['attributes']['status']
+        dict['id'] = data['relationships']['trip']['data']['id']
         list.append(dict)
     
-    i = 0
     for data in response['included']:
-        if i == len(list):
-            i = 0
-        if data['type'] == 'trip':
-            # get train ids
-            list[i]['train'] = data['attributes']['name']
-            #get destinations
-            list[i]['destination'] = data['attributes']['headsign']
-        if data['type'] == 'schedule':
-            #get departure times
-            list[i]['time'] = data['attributes']['departure_time']
-        i+=1
+        for el in list:
+            if data['type'] == 'trip' and el['id'] == data['id']:
+                # get train ids
+                el['train'] = data['attributes']['name']
+                #get destinations
+                el['destination'] = data['attributes']['headsign']
+            if data['type'] == 'schedule' and el['id'] in data['id']:
+                #get departure times
+                el['time'] = data['attributes']['departure_time']
 
     # sort list by time
     for i in range(0, len(list)):
@@ -50,18 +48,13 @@ def board(request):
 
     # format time 
     for el in list:
-        el['time'] = datetime.fromisoformat(el['time']).astimezone(timezone('America/New_York')).strftime('%I:%M %p')
-               
-
-       
-
-
-
+        el['time'] = datetime.fromisoformat(el['time']).strftime('%I:%M %p')
+        
     cur_time = datetime.now().astimezone(timezone('America/New_York')).strftime('%I:%M:%S %p')
 
     context = {
         'data': list,
-        'time': cur_time
+        'time': cur_time,
     }
 
     return render(request, "board.html", context)
