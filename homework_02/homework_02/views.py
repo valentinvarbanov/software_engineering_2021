934f6eda7d3b176e1875d6120ec39b63a116229b
DESKTOP-3APDL85\USER-1 <atanas.p.atanasov.2017@elsys-bg.org>
2021-10-30 13:49:49 +0300

    Fixed timetable

--- homework_02/homework_02/views.py
+++ homework_02/homework_02/views.py
@@ -6,33 +6,50 @@ from . import templates
 import requests
 import datetime
 
-#import requests
-
 def timetable(request):
     response = requests.get("https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=-departure_time&filter%5Bstop%5D=place-north")
     raw_data = response.json()
-    routes = []
+    arrivals = []
+    destinations = []
+    trainNums = []
     statuses = []
 
     for i in raw_data['data']:
         try:
-            route = requests.get("https://api-v3.mbta.com/stops/" + i["relationships"]["stop"]["data"]["id"]).json()["data"]["attributes"]["description"]
-            routes.append(route)
+            arrival = i['attributes']['arrival_time']
+            cut = arrival.split('T',1)[1]
+            time = cut.split('-')[0]
+            arrivals.append(time)
         except:
-            routes.append("No route")
+            arrivals.append("None")
 
     for i in raw_data['data']:
         try:
-            status = requests.get("https://api-v3.mbta.com/vehicles/" + i["relationships"]["vehicle"]["data"]["id"]).json()["data"]["attributes"]["current_status"]
-            statuses.append(status)
-
+            destination = requests.get("https://api-v3.mbta.com/trips/" + i["relationships"]["trip"]["data"]["id"]).json()["data"]["attributes"]["headsign"]
+            destinations.append(destination)
         except:
-            statuses.append("No status")
+            destinations.append("No destination")
+
+    for i in raw_data['data']:
+        try:
+            trainNum = requests.get("https://api-v3.mbta.com/vehicles/" + i['relationships']['vehicle']['data']['id']).json()['data']['attributes']['label']
+            trainNums.append(trainNum)
+        except:
+            trainNums.append("No info")
+
+    for i in raw_data['data']:
+        try:
+            status = i['attributes']['status']
+            statuses.append(status)
+        except:
+            statuses.append("No info")
 
     now = datetime.datetime.now()
     context = {
         'data': raw_data,
-        'routes': routes,
+        'arrivals': arrivals,
+        'destinations': destinations,
+        'trainNums': trainNums,
         'statuses': statuses,
         'day': now.strftime("%A"),
         'date': now.strftime("%m - %d - %Y")
