b26cccbfcbded04e9bf8e6397482e322f68b3007
Vic-Dim <vdimitrov5940@gmail.com>
2021-11-01 01:04:49 +0200

    Some changes

--- homework_02/victor_dimitrov_04/views.py
+++ homework_02/victor_dimitrov_04/views.py
@@ -12,7 +12,7 @@ def trains(request):
         'page[offset]': '0',
         'page[limit]': '50',
         'include': 'trip,schedule',
-        'filter[stop]': 'place-north',
+        'filter[stop]':'place-north',
         'filter[route_type]': '2',
     }
 
@@ -25,15 +25,27 @@ def trains(request):
         train_info['status'] = element['attributes']['status']
         train_info['route'] = element['relationships']['route']['data']['id'][3:]
 
+        #I am lazy person - the numbers on those indices attended both in the route id and the track id
+        #That indicated those numbers were connected to something, which after some search appeared to be
+        #the train/track/vehicle number.  
+        train_info['track'] = element['relationships']['trip']['data']['id'][10:14]
+       
+
+        #Instead of searching in the monstrosity of lists and dicts in the response I preferred to
+        #send request with filters. The filters will significantly reduce the number of schedules, and thus,
+        #the amount of data, that is sent back to the django server.
         time_filters = {
             'include': 'prediction',
-            'filter[route]': element['relationships']['route']['data']['id'],
-            'filter[direction_id]' : 0
+            'filter[trip]': element['relationships']['trip']['data']['id'],
+            'filter[stop]':'place-north',
+            'filter[route_type]': '2'
         }
 
         time_response = requests.get('https://api-v3.mbta.com/schedules', params=time_filters).json()
 
-        train_info['time'] = time_response['data'][0]['attributes']['departure_time'][11:16]
+        train_info['time'] = time_response['data'][0]['attributes']['arrival_time'] or time_response['data'][0]['attributes']['departure_time']
+        train_info['time'] = train_info['time'][11:16]
+
         trains.append(train_info)
 
     data = {}
