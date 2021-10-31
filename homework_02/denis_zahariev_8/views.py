aa451bdf828091bb266bb1a9863eb1f820af1264
Denis Zahariev <denis.zaharievv@gmail.com>
2021-10-31 19:12:05 +0200

    final changes

--- homework_02/denis_zahariev_8/views.py
+++ homework_02/denis_zahariev_8/views.py
@@ -8,34 +8,51 @@ import datetime
 import itertools
 import pytz
 def index(request): 
+
+    params = {
+        "sort" : "-arrival_time",
+        "filter[direction_id]" : "0",
+        "page[offset]" : "0",
+        "include" : "trip,schedule",
+        "page[limit]" : "100",
+        "filter[route_type]" : "2",
+        "filter[stop]" : "place-north",
+        "filter[direction_id]" : "0",
+    }
+
     now = datetime.datetime.now(pytz.timezone('America/New_York'))
-    response = requests.get("https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=-departure_time&filter%5Bstop%5D=place-north", headers={"X-API-Key":"537204397425447493511cec18bfc0fa"})
-    vehicle_routes, vehicle_status = list(), list()
-    data = response.json()
+    response = requests.get('https://api-v3.mbta.com/predictions', params=params).json()
 
-    for i in data['data']:
-        try:
-            route = requests.get("https://api-v3.mbta.com/stops/" + i["relationships"]["stop"]["data"]["id"]).json()["data"]["attributes"]["description"]
-            vehicle_routes.append(route)
-        except:
-            vehicle_routes.append("No route")
+    table_content = []
+    
+    for data in response['data']:
+        record = dict()
+        record['status'] = data['attributes']['status']
+        rel = data['relationships']
+        record['tripID'] = rel['trip']['data']['id']
+        record['stopID'] = rel['stop']['data']['id']
+        table_content.append(record)
+    
+    for data in response['included']:
+        for row in table_content:
+            if data['type'] == 'trip':
+                if row['tripID'] == data['id']:
+                    row['destination'] = data['attributes']['headsign']
+                    row['train'] = data['attributes']['name']
+                    break
+            elif data['type'] == 'schedule':
+                if row['tripID'] in data['id']:
+                    dest_time = datetime.datetime.fromisoformat(str(data['attributes']['departure_time'])).strftime("%I:%M %p").upper()
+                    row['time'] = dest_time
+                    break
+            elif data['type'] == 'stop':
+                if row['stopID'] == data['id']:
+                    if data['attributes']['platform_code'] is None or len(data['attributes']['platform_code'])<1:
+                        row['track'] = 'TBD'
+                    else: 
+                        row['track'] = data['attributes']['platform_code']
 
-        try:
-            status = requests.get("https://api-v3.mbta.com/vehicles/" + i["relationships"]["vehicle"]["data"]["id"]).json()["data"]["attributes"]["current_status"]
-            vehicle_status.append(status)
-        except:
-            vehicle_status.append("No status")
-
-
-    table_content = list()
-
-    for i, val in enumerate(data['data']):
-        table_content.append([
-                            val['attributes']['arrival_time'].replace("T",'\n') if val['attributes']['arrival_time'] != None else None, 
-                            val['attributes']['departure_time'].replace("T",'\n') if val['attributes']['departure_time'] != None else None, 
-                            val['relationships']['vehicle']['data']['id'],
-                            vehicle_routes[i],
-                            vehicle_status[i]])
+    table_content = sorted(table_content, key = lambda i: i['time'])
 
     context = {
         'data': table_content,
