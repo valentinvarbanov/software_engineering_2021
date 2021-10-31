07d3bb65cb05e0d37f410a851f9617a9a65019d9
Vic-Dim <vdimitrov5940@gmail.com>
2021-11-01 01:50:20 +0200

    Some changes

--- homework_02/victor_dimitrov_04/views.py
+++ homework_02/victor_dimitrov_04/views.py
@@ -19,6 +19,7 @@ def trains(request):
     response = requests.get('https://api-v3.mbta.com/predictions', params=initial_filters).json()
 
     trains = []
+    trip_ids = []
 
     for element in response['data']:
         train_info = {}
@@ -27,23 +28,18 @@ def trains(request):
 
         #I am lazy person - the numbers on those indices attended both in the route id and the track id
         #That indicated those numbers were connected to something, which after some search appeared to be
-        #the train/track/vehicle number.  
-        train_info['track'] = element['relationships']['trip']['data']['id'][10:14]
-       
+        #the train/track/vehicle number. 
+        trip_id = element['relationships']['trip']['data']['id']
+        trip_ids.append(trip_id)
+        train_info['track'] = trip_id[10:14]
 
-        #Instead of searching in the monstrosity of lists and dicts in the response I preferred to
-        #send request with filters. The filters will significantly reduce the number of schedules, and thus,
-        #the amount of data, that is sent back to the django server.
-        time_filters = {
-            'include': 'prediction',
-            'filter[trip]': element['relationships']['trip']['data']['id'],
-            'filter[stop]':'place-north',
-            'filter[route_type]': '2'
-        }
+        #I hate this part
+        for departure in response['included']:
+            if departure['type'] == 'schedule':
+                for id in trip_ids:
+                    if departure['relationships']['trip']['data']['id'] == id:
+                        train_info['time'] = departure['attributes']['arrival_time'] or departure['attributes']['departure_time']
 
-        time_response = requests.get('https://api-v3.mbta.com/schedules', params=time_filters).json()
-
-        train_info['time'] = time_response['data'][0]['attributes']['arrival_time'] or time_response['data'][0]['attributes']['departure_time']
         train_info['time'] = train_info['time'][11:16]
 
         trains.append(train_info)
@@ -51,5 +47,6 @@ def trains(request):
     data = {}
     data["context"] = trains
 
+    #departure['attributes']['arrival_time'] or departure['attributes']['departure_time']
 
     return render(request, "index_trains.html", data)
