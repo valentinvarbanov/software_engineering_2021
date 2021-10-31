2ed73c8739c4ebf8a3ca85ffe1cfd68b5039751e
Ivan-Enchev <72374922+Ivan-Enchev@users.noreply.github.com>
2021-10-31 21:12:40 +0200

    fixed
    doing edit because git commit doesn't work

--- homework_02/ivan_enchev_10/apps.py
+++ homework_02/ivan_enchev_10/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class IvanEnchev10Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ivan_enchev_06'
+    name = 'ivan_enchev_10'
