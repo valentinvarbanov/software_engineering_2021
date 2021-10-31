21e2dbc267869c4e6ac748311d394a82e80dbd93
Ivan-Enchev <72374922+Ivan-Enchev@users.noreply.github.com>
2021-10-31 21:14:13 +0200

    fixed
    doing edit because git commit doesn't work

--- homework_02/ivan_enchev_10/views.py
+++ homework_02/ivan_enchev_10/views.py
@@ -4,11 +4,48 @@ import pytz
 import datetime
 
 def departure_board(request):
-    train_status = []
-    json_data = requests.get("https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=-departure_time&filter%5Bstop%5D=place-north").json()["data"]
 
-    for status in json_data:
-        train_status.append(status["attributes"]["status"])
+    params = {
+        "include": "trip,schedule",
+        "filter[stop]": "place-north",
+        "filter[direction_id]": "0",
+        "filter[route_type]": "2",
+        "sort": "-arrival_time",
+        "page[offset]": "0",
+        "page[limit]": "100",
+    }
+
+    trips = []
+    trains = []
+    directions = []
+    destination_times = []
+    train_statuses = []
+
+    json_data = requests.get("https://api-v3.mbta.com/predictions", params=params).json()
+
+    for trip_info in json_data["data"]:
+        trips.append(trip_info["relationships"]["trip"]["data"]["id"])
+
+    for train_info in json_data["included"]:
+        if train_info["type"] == "trip":
+            for id in trips:
+                if train_info["id"] == id:
+                    trains.append(train_info["attributes"]["name"])
+
+    for direction_info in json_data["included"]:
+        if direction_info["type"] == "trip":
+            for id in trips:
+                if direction_info["id"] == id:
+                    directions.append(direction_info["attributes"]["headsign"])
+
+    for time_info in json_data["included"]:
+        if time_info["type"] == "schedule":
+            for id in trips:
+                if time_info["relationships"]["trip"]["data"]["id"] == id:
+                    destination_times.append(datetime.datetime.fromisoformat(str(time_info["attributes"]["departure_time"])).strftime("%I:%M %p"))
+
+    for status in json_data["data"]:
+        train_statuses.append(status["attributes"]["status"])
 
     date = datetime.datetime.now(pytz.timezone("America/New_York"))
     context = {
@@ -16,7 +53,11 @@ def departure_board(request):
         "month": date.month,
         "year": date.year,
         "now": date.strftime("%H:%M:%S"),
-        "train_statuses": train_status,
+        "trips": trips,
+        "trains": trains,
+        "directions": directions,
+        "destination_times": destination_times,
+        "train_statuses": train_statuses,
         "data": json_data
     }
 
