5d005c30a7caf2df98786ea85603ec81962e6c7f
Ivan-Enchev <72374922+Ivan-Enchev@users.noreply.github.com>
2021-11-09 16:31:58 +0200

    Update settings.py

--- homework_02/homework_02/settings.py
+++ homework_02/homework_02/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ivan_enchev_10'
+    'ivan_enchev_10',
 ]
 
 MIDDLEWARE = [
