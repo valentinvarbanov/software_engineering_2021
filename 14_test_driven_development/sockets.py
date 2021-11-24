b0dc7e987fb3e187df37a365bb788c587b8a1f2c
Teodor1331 <72693824+Teodor1331@users.noreply.github.com>
2021-11-24 17:13:51 +0200

    TDD from 24.11.21 (#28)
    Co-authored-by: GerganaRoeva <37037382+GerganaRoeva@users.noreply.github.com>

--- 14_test_driven_development/sockets.py
+++ 14_test_driven_development/sockets.py
@@ -16,3 +16,32 @@ class IPAdress:
 
     def is_public(self):
         return not self.is_private()
+
+class SocketAddress:
+    __MIN_PORT = 0
+    __MAX_PORT = 2 ** 16 - 1
+
+    def __init__(self, address):
+        assert isinstance(address, str)
+
+        self.__address = address
+
+        if len(self.address.split(':')) == 2 and self.address.split(':')[1]:
+            self.__port = int(self.__address.split(':')[1])
+        
+
+    @property
+    def address(self):
+        return self.__address
+
+
+    @property
+    def port(self):
+        return self.__port
+
+
+    def is_valid(self):
+        return SocketAddress.__MIN_PORT <= self.port <= SocketAddress.__MAX_PORT
+
+
+    #def is_whole_socket_address(self):
