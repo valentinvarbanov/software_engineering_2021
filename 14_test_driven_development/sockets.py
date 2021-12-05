be6043585f960f61ea7a81e6c99582754e5ac120
GerganaRoeva <37037382+GerganaRoeva@users.noreply.github.com>
2021-12-05 17:32:33 +0200

    Circles.v2 (#48)
    * sample solution for hw2
    
    * UI class work
    
    * cw tdd
    
    * hw03
    
    * fixed logic
    
    Co-authored-by: Valentin Varbanov <valentin.varbanov@gmail.com>

--- 14_test_driven_development/sockets.py
+++ 14_test_driven_development/sockets.py
@@ -5,10 +5,11 @@ class IPAdress:
     def __init__(self, ip) -> None:
         self.ip = ip
 
-    def is_valid(self) -> bool:
+    def is_valid(self):
         regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-        return re.search(regex, self.ip) != None
- 
+        return re.search(regex, self.ip)
+
+
     def is_private(self):
         # return self.is_valid() and (self.ip[0:3]=='10.' or self.ip[0:7]=='192.168' or self.ip[0:4]=='172.')
         regex = '^(10(\\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){3}|((172\\.(1[6-9]|2[0-9]|3[01]))|192\\.168)(\\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){2})$'
@@ -52,4 +53,3 @@ class SocketAddress:
 
 
     #def is_whole_socket_address(self):
-
--- homework_03/07_gergana_roeva/test_hw03.py
+++ homework_03/07_gergana_roeva/test_hw03.py
@@ -5,28 +5,21 @@ def test_same_position():
     other_c = Circle(Point(4.5, 3.3), 6.0)
     assert  circle.find_relative_position(other_c) == RelativePosition.SAME
 
-# not inside 
-def test_touching_case1():
-    circle = Circle(Point(0.0, 0.0), 6.0)
-    other_c = Circle(Point(9.0, 0.0), 3.0)
-    assert  circle.find_relative_position(other_c) == RelativePosition.TOUCHING
-
 # inside 
-def test_touching_case2():
-    circle = Circle(Point(0.0, 0.0), 6.0)
-    other_c = Circle(Point(-5.0, 0.0), 1.0)
-    assert  circle.find_relative_position(other_c) == RelativePosition.TOUCHING
-
-# not inside 
-def test_no_common_points_case1():
-    circle = Circle(Point(0.0, 0.0), 6.0)
-    other_c = Circle(Point(0.0, -9.0), 1.0)
-    assert  circle.find_relative_position(other_c) == RelativePosition.NO_COMMON_POINTS
-
-# inside 
-def test_no_common_points_case2():
+def test_same_case2():
     circle = Circle(Point(0.0, 0.0), 6.0)
     other_c = Circle(Point(0.0, 0.0), 1.0)
+    assert  circle.find_relative_position(other_c) == RelativePosition.SAME
+
+# inside 
+def test_same_case3():
+    circle = Circle(Point(0.0, 0.0), 6.0)
+    other_c = Circle(Point(-5.0, 0.0), 1.0)
+    assert  circle.find_relative_position(other_c) == RelativePosition.SAME
+
+def test_no_common_points():
+    circle = Circle(Point(0.0, 0.0), 6.0)
+    other_c = Circle(Point(0.0, -9.0), 1.0)
     assert  circle.find_relative_position(other_c) == RelativePosition.NO_COMMON_POINTS
 
 def test_intersecting():
