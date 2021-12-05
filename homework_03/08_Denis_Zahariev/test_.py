c3f87bb9a69cc5c9934ac766a36dabe34ccb334f
denis <deni@>
2021-12-05 16:32:17 +0200

    Implemented suggestions from code reviews

--- homework_03/08_Denis_Zahariev/test_.py
+++ homework_03/08_Denis_Zahariev/test_.py
@@ -1,18 +1,18 @@
 from circle import Point, Circle, RelativePostion 
 
 # test for point initialization
-def create_point():
-    point = Point(3, 2)
+def test_create_point():
+    point = Point(3.0, 2.0)
     assert isinstance(point, Point)
 
 # test for circle initialization
-def create_circle() -> None:
-    center = Point(3, 3)
-    r = 1
+def test_create_circle():
+    center = Point(3.0, 3.0)
+    r = 1.0
     circle = Circle(center, r)
     assert isinstance(circle, Circle)
 
-def test_same() -> None:
+def test_same():
 
     circle1 = Circle(Point(6.0, 6.0), 6.0)
 
@@ -23,13 +23,7 @@ def test_same() -> None:
 
     assert circle1.find_relative_position(circle2) == RelativePostion.SAME
 
-    circle3 = Circle(Point(6.0, 6.0), 5.0)
-
-    assert circle1.find_relative_position(circle3) != RelativePostion.SAME
-
-
-
-def test_no_common_points() -> None:
+def test_no_common_points():
 
     circle1 = Circle(Point(0.0, 0.0), 1.0)
     circle2 = Circle(Point(3.0, 3.0), 1.0)
@@ -41,8 +35,13 @@ def test_no_common_points() -> None:
 
     assert circle1.find_relative_position(circle2) == RelativePostion.NO_COMMON_POINTS
 
+    circle1 = Circle(Point(6.0, 6.0), 6.0)
+    circle2 = Circle(Point(6.0, 6.0), 5.0)
 
-def test_touching() -> None:
+    assert circle1.find_relative_position(circle2) == RelativePostion.NO_COMMON_POINTS
+
+
+def test_touching():
 
     # external touch
     circle1 = Circle(Point(3.0, 3.0), 3.0)
@@ -56,14 +55,9 @@ def test_touching() -> None:
 
     assert circle3.find_relative_position(circle4) == RelativePostion.TOUCHING
 
-def test_intersecting() -> None:
+def test_intersecting():
 
     circle1 = Circle(Point(1.0, 1.0), 3.0)
     circle2 = Circle(Point(2.0, 1.0), 3.0)
 
     assert circle1.find_relative_position(circle2) == RelativePostion.INTERSECTING
-
-test_same()
-test_no_common_points()
-test_touching()
-test_intersecting() 
