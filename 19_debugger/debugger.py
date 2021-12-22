b8824cc720d82e53ffdc28e06ca0ceafd9296009
Valentin Varbanov <valentin.varbanov@gmail.com>
2021-12-22 08:24:55 +0200

    Add debugger materials and demo

---
+++ 19_debugger/debugger.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from datetime import date
+
+def func():
+    print("Hello")
+
+
+def greet():
+    current_date = datetime.now()
+    print(current_date)
+    return greet_with_date(current_date)
+
+def greet_with_date(date):
+    if date.month == 12 and date.day >= 24 and date.day <= 26:  
+        return "Merry Christmas!"
+    return "Good morning"
+
+datee = datetime.now()
+print(greet_with_date(datee))
