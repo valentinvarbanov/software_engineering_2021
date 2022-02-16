e3789a16c1c968da39d98a0ec3336493b9b08619
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2022-02-16 20:06:34 +0200

    Making last changes according to the teacher

--- homework_04/15_Martin_Dinev/test_point.py
+++ homework_04/15_Martin_Dinev/test_point.py
@@ -1,6 +1,13 @@
+"""
+File to test if everything is correct
+"""
+
 from point import Circle, Point, RelativePosition
 
 def test_no_common_points():
+    """
+    Test if two circles have no common points
+    """
     circle1 = Circle(Point(0.0, 0.0), 3.0)
     circle2 = Circle(Point(10.0, 0.0), 4.0)
     assert circle1.find_relative_position(circle2) == RelativePosition.NO_COMMON_POINTS
@@ -10,6 +17,9 @@ def test_no_common_points():
     assert circle1.find_relative_position(circle2) == RelativePosition.NO_COMMON_POINTS
 
 def test_touching():
+    """
+    Test if two circles are touching
+    """
     circle1 = Circle(Point(0.0, 0.0), 4.0)
     circle2 = Circle(Point(10.0, 0.0), 6.0)
     assert circle1.find_relative_position(circle2) == RelativePosition.TOUCHING
@@ -19,6 +29,9 @@ def test_touching():
     assert circle1.find_relative_position(circle2) == RelativePosition.TOUCHING
 
 def test_same():
+    """
+    Test if two circles are the same
+    """
     circle1 = Circle(Point(0.0, 0.0), 6.0)
     circle2 = Circle(Point(0.0, 0.0), 4.0)
     assert circle1.find_relative_position(circle2) == RelativePosition.SAME
@@ -36,6 +49,9 @@ def test_same():
     assert circle1.find_relative_position(circle2) == RelativePosition.SAME
 
 def test_intersecting():
+    """
+    Test if two circles are intersecting
+    """
     circle1 = Circle(Point(0.0, 0.0), 5.0)
     circle2 = Circle(Point(10.0, 0.0), 7.0)
     assert circle1.find_relative_position(circle2) == RelativePosition.INTERSECTING
