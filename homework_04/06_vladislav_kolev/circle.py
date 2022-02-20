ecfafb8ca74c553c00abca9fb4a521bf1c66eb3c
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-02-20 14:13:28 +0200

    modified circle file

--- homework_04/06_vladislav_kolev/circle.py
+++ homework_04/06_vladislav_kolev/circle.py
@@ -1,39 +1,56 @@
-from enum import Enum, auto
-from math import sqrt, pow
+"""
+This file finds the relative position of two circles
+"""
+
+# pylint: disable=R0903
+from enum import Enum
+from math import sqrt
 
 class RelativePosition(Enum):
+    """
+    Enum for the types of relation between circles
+    """
     NO_COMMON_POINTS = 1
     TOUCHING = 2
     INTERSECTING = 3
     SAME = 4
 
 class Point:
-    def __init__(self, x, y):
-        assert isinstance(x, float) and isinstance(y, float)
-        self.x = x
-        self.y = y
+    """
+    Class that defines a point
+    """
+    def __init__(self, x_coord, y_coord):
+        assert isinstance(x_coord, float) and isinstance(y_coord, float)
+        self.x_coord = x_coord
+        self.y_coord = y_coord
 
 
 class Circle:
-    def __init__(self, center, r):
-        assert isinstance(center, Point) and isinstance(r, float)
+    """
+    Class that defines a circle
+    """
+    def __init__(self, center, radius):
+        assert isinstance(center, Point) and isinstance(radius, float)
         self.center = center
-        self.r = r
+        self.radius = radius
 
     def find_relative_position(self, other_circle):
+        """
+        Function that finds relative postion of one circle to another
+        """
         assert isinstance(other_circle, Circle)
 
-        centers_distance = sqrt( pow((self.center.x - other_circle.center.x), 2) + pow((self.center.y - other_circle.center.y), 2) )
+        centers_distance = sqrt((self.center.x_coord - other_circle.center.x_coord)**2
+                + (self.center.y_coord - other_circle.center.y_coord)**2)
 
-        if (self.center.x == other_circle.center.x and self.center.y == other_circle.center.y
-             and self.r == other_circle.r):
+        if (self.center.x_coord == other_circle.center.x_coord
+             and self.center.y_coord == other_circle.center.y_coord
+             and self.radius == other_circle.r):
             return RelativePosition.SAME
-        elif (abs(self.r - other_circle.r) < centers_distance <  self.r + other_circle.r):
+        if abs(self.radius - other_circle.r) < centers_distance <  self.radius + other_circle.r:
             return RelativePosition.INTERSECTING
-        elif centers_distance == self.r + other_circle.r or centers_distance == (abs(self.r - other_circle.r)):
+        if centers_distance == self.radius + other_circle.r \
+                         or centers_distance == (abs(self.radius - other_circle.r)):
             return RelativePosition.TOUCHING
-        else:
-            return RelativePosition.NO_COMMON_POINTS
 
-
-
+        return RelativePosition.NO_COMMON_POINTS
