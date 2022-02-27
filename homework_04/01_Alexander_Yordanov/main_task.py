295013ce586e6e17f0166c804bbe5aa80c69a7ba
Xander <alexanderfjor@gmail.com>
2022-02-27 21:00:02 +0200

    File for hw04
    File for hw04

--- homework_04/01_Alexander_Yordanov/main_task.py
+++ homework_04/01_Alexander_Yordanov/main_task.py
@@ -1,11 +1,23 @@
+#pylint: disable=R0903
+#pylint: disable=C0303
+#pylint: disable=C0103
+
 import math
 from enum import Enum
+
+'''
+Enum types for all the positions 
+'''
 
 class ReturnTypes(Enum):
     NO_COMMON_POINTS = 1
     TOUCHING = 2
     INTERSECTING = 3
     SAME = 4
+
+'''
+Point object 
+'''
 
 class Point: 
     def __init__(self, x, y) -> None:
@@ -15,28 +27,44 @@ class Point:
         self.x = x
         self.y = y
 
+'''
+Circle object 
+'''
+
 class Circle:
-    def __init__(self, center, r) -> None:
+    def __init__(self, center, radius) -> None:
         assert isinstance(center, Point)
 
         self.center = center
-        self.r = r
+        self.radius = radius
+
+    '''
+    Find the relative position function 
+    '''
 
     def find_relative_position(self, another_circle):
         assert isinstance(another_circle, Circle)
 
-        distance = math.sqrt((another_circle.center.x - self.center.x) ** 2 + (another_circle.center.y - self.center.y) ** 2)
-        radiusSum = self.r + another_circle.r
-        radiusSub = self.r - another_circle.r
+        distance = math.sqrt((another_circle.center.x - self.center.x) ** 2 + \
+            (another_circle.center.y - self.center.y) ** 2)
+        
+        sumOfTheRadius = self.radius + another_circle.radius
+        subOfTheRadius = self.radius - another_circle.radius
 
-        if self.center.x == another_circle.center.x and self.center.y == another_circle.center.y or abs(distance) < abs(radiusSub):
+
+        '''
+        All the case we have to check for positions.
+        '''
+
+        if self.center.x == another_circle.center.x and self.center.y == another_circle.center.y \
+            or abs(distance) < abs(subOfTheRadius):
             return ReturnTypes.SAME
 
-        elif abs(distance) > radiusSum:
+        elif abs(distance) > sumOfTheRadius:
             return ReturnTypes.NO_COMMON_POINTS
 
-        elif abs(distance) == radiusSum or abs(distance) == abs(radiusSub):
+        elif abs(distance) == sumOfTheRadius or abs(distance) == abs(subOfTheRadius):
             return ReturnTypes.TOUCHING
 
-        elif abs(radiusSub) < abs(distance) < radiusSum:
+        elif abs(subOfTheRadius) < abs(distance) < sumOfTheRadius:
             return ReturnTypes.INTERSECTING
