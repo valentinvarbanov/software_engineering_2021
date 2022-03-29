2485fba672727ff08f023212ee0b1dcd2350b13d
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2022-03-29 10:15:48 +0300

    Adding hw for martin dinev

---
+++ homework_05/15_martin_dinev/decrypt.py
@@ -0,0 +1,33 @@
+from posixpath import split
+from nltk.corpus import wordnet
+
+key = 0
+str = input()
+
+for i in range(0, -25, -1):
+    for word in str.split():
+        correctWord = ""
+        for char in word:
+            if (64 < ord(char) < 91 or 96 < ord(char) < 123):
+                correctWord += chr(ord(char) + i)
+        if not wordnet.synsets(correctWord):
+            break
+        else:
+            if word == str.split()[len(str.split())-1]:
+                key = -i
+
+newStr = ""
+for word in str.split():
+    correctWord = ""
+    for char in word:
+        if (64 < ord(char) < 91 or 96 < ord(char) < 123):
+            correctWord += chr(ord(char) - key)
+        else:
+            correctWord += char
+    if not word == str.split()[len(str.split())-1]:
+        newStr += correctWord + " "
+    else:
+       newStr += correctWord 
+
+print(newStr)
+print(key)
