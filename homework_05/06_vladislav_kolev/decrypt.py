4749c0c625edc40d00487c13a17f509e6753de61
Vladislav Kolev <48590126+Vladikolev0321@users.noreply.github.com>
2022-04-13 16:30:05 +0300

    06 vladislav kolev (#136)
    * Add homework
    
    * Add right output format
    
    * Modified file
    
    * final modification

--- homework_05/06_vladislav_kolev/decrypt.py
+++ homework_05/06_vladislav_kolev/decrypt.py
@@ -48,15 +48,16 @@ def decrypt_text():
     input_text = input()
     input_words = input_text.split(' ')
 
-    for key in range(26):
+   for key in range(26):
         result_words = []
+        count_decrypted = 0
         for word in input_words:
             decrypted_word = decrypt_word(word, key)
+            result_words.append(decrypted_word)
             if check_is_word_valid(decrypted_word):
-                result_words.append(decrypted_word)
-            else:
-                break
-        if len(result_words) >= len(input_words)/2:
+                count_decrypted += 1
+            
+        if count_decrypted >= len(input_words)/2:
             print(concat_result(result_words))
             print("key: "+ str(key))
             return
