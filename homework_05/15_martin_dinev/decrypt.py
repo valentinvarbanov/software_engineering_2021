651c8886e932dcdc3aa22cfae7c5bc48b0e0fbb1
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2022-04-19 16:36:26 +0300

    add maybe fixed homework

--- homework_05/15_martin_dinev/decrypt.py
+++ homework_05/15_martin_dinev/decrypt.py
@@ -1,33 +1,53 @@
-from posixpath import split
 from nltk.corpus import wordnet
 
 key = 0
 str = input()
 
+A_INTEGER = ord('A')
+Z_INTEGER = ord('Z')
+
+a_INTEGER = ord('a')
+z_INTEGER = ord('z')
+
+difference = (Z_INTEGER - A_INTEGER) + 1
+
 for i in range(0, -25, -1):
+    counter = 0
     for word in str.split():
         correctWord = ""
         for char in word:
-            if (64 < ord(char) < 91 or 96 < ord(char) < 123):
-                correctWord += chr(ord(char) + i)
-        if not wordnet.synsets(correctWord):
-            break
-        else:
-            if word == str.split()[len(str.split())-1]:
-                key = -i
+            symbol_integer = ord(char)
+            result = symbol_integer
+
+            if A_INTEGER <= symbol_integer <= Z_INTEGER:
+                result += i
+                if not (A_INTEGER <= result <= Z_INTEGER):
+                    result += difference
+            elif a_INTEGER <= symbol_integer <= z_INTEGER:
+                result += i
+                if not (a_INTEGER <= result <= z_INTEGER):
+                    result += difference
+            correctWord += chr(result)
+        if wordnet.synsets(correctWord):
+            counter += 1
+    if counter > (60/100)*len(str.split()):
+        key = -i
+        break
 
 newStr = ""
-for word in str.split():
-    correctWord = ""
-    for char in word:
-        if (64 < ord(char) < 91 or 96 < ord(char) < 123):
-            correctWord += chr(ord(char) - key)
-        else:
-            correctWord += char
-    if not word == str.split()[len(str.split())-1]:
-        newStr += correctWord + " "
-    else:
-       newStr += correctWord 
+for symbol in str:
+    symbol_integer = ord(symbol)
+    result = symbol_integer
+
+    if A_INTEGER <= symbol_integer <= Z_INTEGER:
+        result -= key
+        if not (A_INTEGER <= result <= Z_INTEGER):
+            result += difference
+    elif a_INTEGER <= symbol_integer <= z_INTEGER:
+        result -= key
+        if not (a_INTEGER <= result <= z_INTEGER):
+            result += difference
+    newStr += chr(result)
 
 print(newStr)
 print("key:", key)
