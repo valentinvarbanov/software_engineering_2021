7813ffecbe4de5bd68c8ec7a5ec4dddaad812c8d
petar <petar.dmnv@gmail.com>
2022-04-27 09:11:15 +0300

    feat: Linter runs properly

--- tasks/continuous_integration/code.py
+++ tasks/continuous_integration/code.py
@@ -1,51 +1,70 @@
+"""
+Code.py
+"""
+
 import os
-import subprocess
 import csv
 
 
-encryption_key_key = 'key'
-text_key = 'data'
-name_key = 'name'
+ENCRYPTION_KEY_KEY = 'key'
+TEXT_KEY = 'data'
+NAME_KEY = 'name'
 
 verification_data = [
     {
-        name_key: 'sample',
-        encryption_key_key : 3,
-        text_key: 'It works!',
+        NAME_KEY: 'sample',
+        ENCRYPTION_KEY_KEY : 3,
+        TEXT_KEY: 'It works!',
     },
     {
-        name_key: 'easy',
-        encryption_key_key : 19,
-        text_key: 'can not go any simpler than this',
+        NAME_KEY: 'easy',
+        ENCRYPTION_KEY_KEY : 19,
+        TEXT_KEY: 'can not go any simpler than this',
     },
     {
-        name_key: 'medium',
-        encryption_key_key : 23,
-        text_key: 'Will CaPiTaLiZaTiOn break the homework',
+        NAME_KEY: 'medium',
+        ENCRYPTION_KEY_KEY : 23,
+        TEXT_KEY: 'Will CaPiTaLiZaTiOn break the homework',
     },
     {
-        name_key: 'special',
-        encryption_key_key : 13,
-        text_key: 'Or $p3c1al characters? Let\'s find out...',
+        NAME_KEY: 'special',
+        ENCRYPTION_KEY_KEY : 13,
+        TEXT_KEY: 'Or $p3c1al characters? Let\'s find out...',
     },
     {
-        name_key: 'advanced',
-        encryption_key_key : 7,
-        text_key: 'AI Dungeon is a free-to-play single-player and multiplayer text adventure game which uses artificial intelligence to generate content. It allows players to create and share their own custom adventure settings. The game\'s first version was made available on Colab in May 2019, and its second version (initially called AI Dungeon 2) was released online and for iOS and Android in December 2019. The AI model was then upgraded in July 2020.',
+        NAME_KEY: 'advanced',
+        ENCRYPTION_KEY_KEY : 7,
+        TEXT_KEY: 'AI Dungeon is a free-to-play\
+         single-player and multiplayer\
+        text adventure game which uses \
+        artificial intelligence to generate \
+        content. It allows players to create\
+         and share their own custom adventure\
+         settings. The game\'s first version was made available on Colab in May\
+          2019, and its second version (initially called AI Dungeon 2) was \
+          released online and for iOS and Android in December 2019. The AI \
+          model was then upgraded in July 2020.',
     }
 ]
 
+#pylint: disable=E0001
+#pylint: disable=R1732
+#pylint: disable=W1514
 output_file = open('./results.csv', 'w')
 writer = csv.writer(output_file)
 header = ['name']
 
 for data_set in verification_data:
-    header.append(data_set[name_key])
-    header.append(f'{data_set[name_key]} key')
+    header.append(data_set[NAME_KEY])
+    header.append(f'{data_set[NAME_KEY]} key')
 
 writer.writerow(header)
 
+
 def get_homeworks():
+    """
+    Function ffor getting homeworks
+    """
     subfolders = [ f.path for f in os.scandir("./") if f.is_dir() ]
     return subfolders
 
@@ -60,30 +79,32 @@ for hw in get_homeworks():
         first = name_components[1].capitalize()
         last = name_components[2].capitalize()
         name = f'{first} {last}'
-    
+
     csv_row = [name]
 
     print(f'----- {name} -----')
 
     for data_set in verification_data:
-        key = data_set[encryption_key_key]
-        text = data_set[text_key]
+        KEY = data_set[ENCRYPTION_KEY_KEY]
+        TEXT = data_set[TEXT_KEY]
 
-        decrypted_output = os.popen(f'echo "{text}" | python encrypt.py --key "{key}" > encrypted && cat encrypted | python {hw}/decrypt.py').read()
+        decrypted_output = os.popen(f'echo "{TEXT}" | \
+        python encrypt.py --key "{KEY}" > encrypted && cat encrypted |\
+         python {hw}/decrypt.py').read()
 
-        decryption_success = 0
-        key_discovered = 0
+        DECRIPTION_SUCCESS = 0
+        KEY_DISCOVERED = 0
 
-        if text in decrypted_output:
-            decryption_success = 1
-        
-        if f'key: {key}' in decrypted_output:
-            key_discovered = 1
+        if TEXT in decrypted_output:
+            DECRIPTION_SUCCESS = 1
 
-        csv_row.append(decryption_success)
-        csv_row.append(key_discovered)
+        if f'key: {KEY}' in decrypted_output:
+            KEY_DISCOVERED = 1
+        csv_row.append(DECRIPTION_SUCCESS)
+        csv_row.append(KEY_DISCOVERED)
 
-        print(f'{data_set[name_key]}: key({key_discovered}) text({decryption_success})')
+        print(f'{data_set[NAME_KEY]}: key({KEY_DISCOVERED}) \
+        text({DECRIPTION_SUCCESS})')
         print(f'==>{decrypted_output}<==')
 
     writer.writerow(csv_row)
